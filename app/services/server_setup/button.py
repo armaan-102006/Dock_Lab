31f01f229ed0f361a1d611b8bc8903a6d3e12f5c
armaan-102006 <ironman.armaan@gmail.com>
2026-06-28 18:25:16 +0530

    may this be a warning to those who try to use subprocess.communicate to have a back and forth with the terminal. for communicate ends the process after sending one onput and getting it's results.i will try to use winpty to fix this mess.

--- app/services/server_setup/button.py
+++ app/services/server_setup/button.py
@@ -3,6 +3,9 @@ import keyring
 import subprocess
 import sys
 import requests
+import atexit
+import re
+from subprocess import TimeoutExpired
 #kill the processess of popen on close
 #add who the token is issued to in auth, host or user?
 root=Tk()
@@ -11,29 +14,65 @@ root.title("server starter")
 p=Entry(root,width=35)
 p.grid(row=0,column=0,columnspan=3)
 
-
+global server_startup,lt_process
 def start_server():
     if connected:
         connected=False
+        kill_process()
     else:
-        port=str(p.get())
         #loc = os.getcwd()
         #server_path = os.path.join(loc,'session_service.py')
-        token=keyring.get_password('dock_lab','access_token')
+        token = keyring.get_password('dock_lab','access_token')
         lt_process = subprocess.Popen(
-            ['lt', '--port', str(port)], 
+            ['ssh', '-R', f'80:localhost:{p.get()}', 'localhost.run'], 
+            stdin=subprocess.PIPE,
             stdout=subprocess.PIPE, 
             stderr=subprocess.PIPE,
             text=True )
-        output_line = lt_process.stdout.readline()
-        url = output_line.replace("your url is: ", "").strip()
+        
+        try:
+            out,err=lt_process.communicate(timeout=1)
+        except TimeoutExpired:
+            lt_process.kill()
+            outs, errs = lt_process.communicate()
+            print("failed to start a server")
+        else:
+            run = keyring.get_password('dock_lab','first_run')
+            if not run:
+                out,err=lt_process.communicate(input='yes\n')
+                keyring.set_password('dock_lab','first_run',"false")
+            if err:
+                try:
+                    out,err=lt_process.communicate(input='ssh-keygen -t rsa -b 4096\n',timeout=1)
+                except TimeoutExpired:
+                    lt_process.kill()
+                    outs, errs = lt_process.communicate()
+                    print("failed to generate ssh key")
+                for i in range(3):
+                    out,err=lt_process.communicate(input='ssh-keygen -t rsa -b 4096\n',timeout=1)
+                    if err:
+                        lt_process.kill()
+                        outs, errs = lt_process.communicate()
+                        print("failed to final setup after key generation")
+
+                outs, errs = lt_process.communicate(input='ssh -R 80:localhost:8080 localhost.run')
+            
+            pattern=r"https://.*?\.lhr\.life"
+            url=re.search(pattern=pattern,string=out)
+
         response = requests.post(
             "server_url/auth/user_auth",
-            headers={"Authorization": f"Bearer {token}"},#get saved tokens issued by backend
+            headers={"Authorization": f"Bearer {token}"},
             json={'url': url}
         )
-        subprocess.Popen([sys.executable, '-m', 'uvicorn', 'main:app', '--reload'])
+        server_startup = subprocess.Popen([sys.executable, '-m', 'uvicorn', 'main:app', '--reload'])
         connected=True
+
+def kill_process():#i am sorry
+    lt_process.terminate()
+    server_startup.terminate()
+
 
 e=Button(root,text='connect',width=35,command=start_server).grid(row=1,column=0,columnspan=3)
 root.mainloop()
+atexit.register(kill_process)
