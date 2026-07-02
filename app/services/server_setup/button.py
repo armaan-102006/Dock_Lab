71b8773f355670397e3141cfbacefd3b71a691e4
armaan-102006 <ironman.armaan@gmail.com>
2026-07-02 13:03:35 +0530

    update button

--- app/services/server_setup/button.py
+++ app/services/server_setup/button.py
@@ -3,8 +3,10 @@ import keyring
 import subprocess
 import sys
 import requests
+import time
 import atexit
 import re
+import threading
 from subprocess import TimeoutExpired
 #kill the processess of popen on close
 #add who the token is issued to in auth, host or user?
@@ -14,65 +16,80 @@ root.title("server starter")
 p=Entry(root,width=35)
 p.grid(row=0,column=0,columnspan=3)
 
-global server_startup,lt_process
-def start_server():
-    if connected:
-        connected=False
-        kill_process()
-    else:
-        #loc = os.getcwd()
-        #server_path = os.path.join(loc,'session_service.py')
+def start_server():#use pesudo terminal instead of subprocess for better results
+    def start_background():
+        global server_startup,lt_process,connected
+        try:
+            if connected:
+                connected=False
+                kill_process()
+                sys.exit()
+        except NameError:
+            connected=False
+
+        output = open('terminal_output.log','w+')
         token = keyring.get_password('dock_lab','access_token')
         lt_process = subprocess.Popen(
             ['ssh', '-R', f'80:localhost:{p.get()}', 'localhost.run'], 
             stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE, 
-            stderr=subprocess.PIPE,
+            stdout=output, 
+            stderr=subprocess.DEVNULL,
             text=True )
-        
-        try:
-            out,err=lt_process.communicate(timeout=1)
-        except TimeoutExpired:
-            lt_process.kill()
-            outs, errs = lt_process.communicate()
-            print("failed to start a server")
-        else:
-            run = keyring.get_password('dock_lab','first_run')
-            if not run:
-                out,err=lt_process.communicate(input='yes\n')
-                keyring.set_password('dock_lab','first_run',"false")
-            if err:
-                try:
-                    out,err=lt_process.communicate(input='ssh-keygen -t rsa -b 4096\n',timeout=1)
-                except TimeoutExpired:
-                    lt_process.kill()
-                    outs, errs = lt_process.communicate()
-                    print("failed to generate ssh key")
-                for i in range(3):
-                    out,err=lt_process.communicate(input='ssh-keygen -t rsa -b 4096\n',timeout=1)
-                    if err:
-                        lt_process.kill()
-                        outs, errs = lt_process.communicate()
-                        print("failed to final setup after key generation")
+        time.sleep(1)
+        output.seek(0)
+        out = output.read()
+        run = keyring.get_password('dock_lab','first_run')
 
-                outs, errs = lt_process.communicate(input='ssh -R 80:localhost:8080 localhost.run')
-            
-            pattern=r"https://.*?\.lhr\.life"
-            url=re.search(pattern=pattern,string=out)
+        if not run:
+            keyring.set_password('dock_lab','first_run',"false")
+            lt_process.stdin.write("yes\n")
+            lt_process.stdin.flush() 
+
+        if not out:         
+        
+            lt_process.terminate()                
+            key_gen = subprocess.Popen(
+            ['ssh-keygen', '-t', 'rsa', '-b', '4096'], 
+            stdin=subprocess.PIPE,
+            stdout=subprocess.DEVNULL, 
+            stderr=subprocess.DEVNULL,
+            text=True )
+            time.sleep(1)
+
+            key_gen.stdin.write("\n\n\n")
+            key_gen.stdin.flush() 
+
+            lt_process = subprocess.Popen(
+            ['ssh', '-R', f'80:localhost:{p.get()}', 'localhost.run'], 
+            stdin=subprocess.PIPE,
+            stdout=output, 
+            stderr=subprocess.DEVNULL,
+            text=True )
+            time.sleep(1)
+            output.seek(0)
+            key_gen.terminate()
+            out = output.read()
+
+        pattern = r"https://.*?\.lhr\.life"
+        url = re.search(pattern=pattern,string=out)
+        if not url:
+            raise RuntimeError("Failed to start tunnel")
 
         response = requests.post(
             "server_url/auth/user_auth",
             headers={"Authorization": f"Bearer {token}"},
-            json={'url': url}
+            json={'url': url[0]}
         )
         server_startup = subprocess.Popen([sys.executable, '-m', 'uvicorn', 'main:app', '--reload'])
         connected=True
-
+    threading.Thread(target=start_background, daemon=True).start()
+    
 def kill_process():#i am sorry
+    global server_startup,lt_process
     lt_process.terminate()
     server_startup.terminate()
 
 
 e=Button(root,text='connect',width=35,command=start_server).grid(row=1,column=0,columnspan=3)
-root.mainloop()
 atexit.register(kill_process)
+root.mainloop()
